8c6c1492295fe43e4b9c5559dedc83a035d32489
Ruba130600 <101045454+Ruba130600@users.noreply.github.com>
2023-10-12 11:55:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import './App.css';
+function App() {
+  
+  var [input, inputSet] = useState('trichy')
+  var [main, mainSet] = useState('Clouds')
+  var [description, descriptionSet] = useState('Broken Clouds')
+  var [temp, tempset] = useState('24')
+  function getData() {
+    console.log()
+    var fetchstatus = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input 
+    + "&appid=ee949cc184264765ad2f53c854eb1a81").then(function (res) {
+      return res.json()
+    })
+    fetchstatus.then(function (data) {
+      var f = data.main.temp
+      var c = f - 273.15;
+      mainSet(data.weather[0]["main"])
+      descriptionSet(data.weather[0]["description"])
+      tempset(Math.floor(c))
+    })
+  }
+  function inputVal(e) {
+    inputSet(e.target.value)
+  }
+  return (
+    <div className="background">
+      <div className="container">
+        <h1 className="wheather"> <b>WEATHER</b></h1>
+        <input type="text" className="Inputcls" placeholder="City Name" id='inputid' onChange={inputVal} value={input} /><br /><br />
+        <button type="button" className="Inputcls submit" onClick={getData} > submit</button>
+        <br />
+        <span className="getdata">{temp}°C </span>
+        <span className="textdata">{main}, {description}</span>
+      </div>
+
+    </div>
+  );
+}
+export default App;
